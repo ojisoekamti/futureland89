89752fa3a442c17ccdc56419e9332e8101324f05
Abdul Ghoji Hanggoro <abdulghoji31@gmail.com>
2022-03-21 09:28:24 +0700

    init

---
+++ app/TypeAccount.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Notifications\Notifiable;
+
+class TypeAccount extends Model
+{
+    use SoftDeletes;
+    use Notifiable;
+
+    /**
+     * For Soft delete
+     * @var array
+     */
+    protected $dates = ['deleted_at'];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->created_by = is_object(Auth::guard(config('app.guards.web'))->user()) ? Auth::guard(config('app.guards.web'))->user()->id : 1;
+            $model->updated_by = NULL;
+        });
+
+        static::updating(function ($model) {
+            $model->updated_by = is_object(Auth::guard(config('app.guards.web'))->user()) ? Auth::guard(config('app.guards.web'))->user()->id : 1;
+        });
+    }
+}
